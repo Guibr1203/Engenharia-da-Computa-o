f1b232c8cc7a43407d4bca58946a539fe2102c96
Fernanda Maria <69825398+fernandacbda@users.noreply.github.com>
2025-05-12 20:55:33 -0300

    Update Calculo Salário.c

--- Calculo Salário.c
+++ Calculo Salário.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 int main (){
     float salario_receber,salario_base,gratificacao,imposto;
-    printf("Digite o valor do Salario:");
+    printf("Digite o valor do Salario:\n");
     scanf("%f", &salario_base);
-    gratificacao=salario_base*5/100;
-    imposto=salario_base*7/100;
+    gratificacao=(salario_base*5)/100;
+    imposto=(salario_base*7)/100;
     salario_receber=salario_base+gratificacao-imposto;
-    printf("Salario a receber e %f", salario_receber);
+    printf("\nSalario a receber e %f", salario_receber);
     return 0;
 }
